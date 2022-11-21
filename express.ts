e194991089ca4814c2b16a65fd0db8196ff46d12
sandeep1-ss <105652583+sandeep1-ss@users.noreply.github.com>
2022-11-22 03:13:20 +0530

    added cors in express handler

--- express.ts
+++ express.ts
@@ -1,8 +1,9 @@
 import express, { RequestHandler } from 'express';
 import { json } from 'body-parser';
 import { router } from './routes';
+import cors from 'cors';
 export const app = express();
 app.use(json() as RequestHandler);
 app.use(express.static('files'));
-
+app.use(cors());
 app.use('/api', router);
